c882fc6884c1bdb1104bda47d13d0dc5a7c06f58
Paul Adams <paadams@nmu.edu>
2016-04-15 18:27:41 -0400

    added login

---
+++ pub/client.js
@@ -0,0 +1,36 @@
+//var socket = io();
+
+$(document).ready(function(){
+    $("#loginPrompt").modal('show');
+    
+    $('#login').on('click', function(){
+        
+        let getUsername = $('#username').val(),
+            getPassword = $('#password').val(),
+            login = {};
+        
+        login.u = getUsername;
+        login.p = getPassword;
+        
+        socket.emit('login', login);
+        
+        $('#loginPrompt').modal('hide');
+        
+    });
+    
+    $('#createAccount').on('click', function(){
+        let createUser = $('#username').val(),
+            createPassword = $("#password").val(),
+            account = {};
+        account.user = createUser;
+        account.pass = createPassword;
+        
+        socket.emit('newuser', account);
+        
+        $('#loginPrompt').modal('hide');
+        
+    });
+        
+	});
+
+
