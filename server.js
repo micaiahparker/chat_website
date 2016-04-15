d09eb33df9a7772b092beb652db6579a8d524329
Micaiah Parker <micaiahparker@gmail.com>
2016-04-15 18:32:54 -0400

    basic server - false login

--- server.js
+++ server.js
@@ -6,9 +6,47 @@ var socketio = require("socket.io");
 var io = socketio(server);
 app.use(express.static("pub"));
 
+var MongoClient = require('mongodb').MongoClient;
+
+function addUser(username, passwd){
+	MongoClient.connect("mongodb://localhost:27017/chat_server", function(err, db) {
+		db.users.insert({'user': username, 'pass': passwd});
+		db.close();
+	});
+}
+
+function newUser(username, passwd){
+	var user = {}
+	user.name = username;
+	user.pass = passwd;
+	user.rooms = {};
+	return user;
+}
+
+function getUser(username, passwd){
+	return {};
+}
+
 io.on("connection", function(socket){
+	socket.on('newuser', function(data){
+		addUser(newUser(data['user'], data['pass']));
+		var user = getUser(data['user'], data['pass']);
+		socket.emit(user);
+	});
+	socket.on('login', function(data){
+		var user = getUser(data['user'], data['pass']);
+		socket.emit(user);
+	});
 });
 
 server.listen(80, function() {
     console.log("Server is listening on port 80");
 });
+
+function testUser(){
+	var user = {}
+	user.name = 'test';
+	user.passwd = 'pass';
+	user.rooms = {};
+	return user;
+}
