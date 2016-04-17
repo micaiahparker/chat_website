a15d4f3d2f3bedc98735b5ec524f8861609a8a44
colpowel <colpowel@nmu.edu>
2016-04-16 20:50:04 -0400

    added some ideas and notes and stuff to client.js

--- pub/client.js
+++ pub/client.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     
     $('#login').on('click', function(){
         
-        let getUsername = $('#username').val(),
+        var getUsername = $('#username').val(),
             getPassword = $('#password').val(),
             login = {};
         
@@ -19,7 +19,7 @@ $(document).ready(function(){
     });
     
     $('#createAccount').on('click', function(){
-        let createUser = $('#username').val(),
+        var createUser = $('#username').val(),
             createPassword = $("#password").val(),
             account = {};
         account.user = createUser;
@@ -38,5 +38,72 @@ $(document).ready(function(){
         });
        
 	});
-
-
+/*
+ * Here are my thoughts on parts of what the client could have/need/use
+ * 
+ * 
+ * User object:
+ *      name
+ *      currentRoom(?)
+ *      List<Room> all available rooms
+ *      id(?)//name is probably id i guess?
+ * 
+ * Room object:
+ *      roomId
+ *      List<Message> localMessages
+ *      List<String> membersInGroup
+ *      
+ * Possible method calls needed:      
+ *      onServerMessage(Room room, Message message)
+ *          this is what the server calls to when it receives a message from someone else
+ *          add message to room's local message list
+ *          if you're in the room, call updateRoom(room) to update your chat box
+ *      onMessage(Message message)
+ *          called when you send a message to your current room
+ *          should add message to local list, call updateRoom(ourUser.room)
+ *          then it should send the message to the server so the server can emit to the other connected clients
+ *          NewMessage{
+ *              room: myUser.currentRoom
+ *              message: message
+ *          }
+ *          socket.emit("newMessage",
+ *      updateRoom(Room r)
+ *          this will update the room's chat log to show all of the local chats
+ *          it needs to call update when you open a new chat and when you receive a new chat(in your room)
+ *      createRoom(UserList usernames)
+ *          sends a call to the server to create a new room with the list of users
+ *          server replies with room's id(?)
+ *          if -1 failed to create room
+ *          otherwise create a new room object and add it to our user's list of available rooms
+ *          or another possibility is the server could reply with the user object, having added
+ *          the room to the list of available rooms to the user server sided anyway maybe?
+ *          newRoom = {
+ *              id: server's reply
+ *              messageList: {}
+ *              userList: usernames
+ *          }
+ *          user.rooms.push(newRoom)
+ *      addUserToRoom(Room r, String username)
+ *          roomRequest = {
+ *              room: r,
+ *              username: username
+ *          }
+ *          socket.emit("addUserToRoom", roomRequest)
+ *          sends a request to the server to add a user to the room
+ *          returns with a success or not(?)
+ *          r.userList.push(username)
+ *          add user to room(?)
+ *          
+ *      removeUserFromRoom(Room r, String username)
+ *          similar to addUserToRoom
+ *      leaveRoom()
+ *          just calls removeUserFromRoom(user.currentRoom, user.username)
+ *          if success, removes current room from list of available rooms and places you in public
+ *          
+ *      enterRoom(Room r)
+ *          this would be when the user clicks to enter a room (or closes room and enters public)
+ *          calls updateRoom(r)
+ *          sets user.currentRoom to r
+ *          tells server joined room r(?)
+ *      
+ */
