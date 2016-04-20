07691b02ab5580038a8dd1f4c368440f333f8e66
Micaiah Parker <micaiahparker@gmail.com>
2016-04-20 18:05:59 -0400

    changed port for ssl

--- server.js
+++ server.js
@@ -86,6 +86,6 @@ io.on("connection", function(socket){
 	});
 });
 
-server.listen(1234, function() {
-    console.log("Server is listening on port 1234");
+server.listen(443, function() {
+    console.log("Server is listening on port 443");
 });
