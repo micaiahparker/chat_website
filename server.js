1fd15ed2203eeef3af05955e105d13d89f4108d5
Micaiah Parker <micaiahparker@gmail.com>
2016-04-21 00:50:53 -0400

    fingers crossed

--- server.js
+++ server.js
@@ -6,69 +6,99 @@ var socketio = require("socket.io");
 var io = socketio(server);
 app.use(express.static("pub"));
 var mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator');
 mongoose.connect('mongodb://localhost/chat_server');
 
-var User = mongoose.model('User', {name: String, pass: String, rooms: Object});
-var Room = mongoose.model('Room', {name: String, log: Array, users: Object});
+var UserSchema = mongoose.Schema({
+	name:{type: String, required: true, unique: true },
+	pass:{type: String, require: true},
+	rooms:{type: Array, require: true}
+});
+
+var RoomSchema = mongoose.Schema({
+	name:{type: String, require: true, unique: true},
+	users:{type: Array},
+	log:{type: Array}
+});
+
+UserSchema.plugin(uniqueValidator);
+RoomSchema.plugin(uniqueValidator);
+
+var User = mongoose.model("User", UserSchema);
+var Room = mongoose.model("Room", RoomSchema);
 
 function newUser(username, passwd, socket){
-	if (!userExists(username)){
-		User.create({name:username, pass:passwd, rooms:{}}, function(err, user){
-			if (!err){
-				socket.emit("senduser", {name:user.name,rooms:user.rooms});
-				console.log("Created user "+ username);
-			} else {
-				badUser(socket);
-				console.log("Couldn't create user "+username);
-			}
-		});
-	} else {
-		badUser(socket);
-		console.log("User already exists "+username);
-	}
+	User.create({name: username, pass: passwd, rooms:{}}, function(err, user){
+		if (!err){
+			//userCreatePass
+			addUserToRoom(user.name, "Public");
+			socket.emit("sendUser", {name:"null", msg:"Account Created"});
+		} else {
+			//userCreateFail
+			socket.emit("sendUser", {name:"null", msg:err});
+		}
+	});
 }
 
 function getUser(username, passwd, socket){
-	if (userExists(username)){
-		User.findOne({name:username, pass:passwd}, 'name rooms', function(err, user){
-			if (!err){
-				socket.emit("senduser", user);
-				console.log("sent: "+ user);
-			} else {
-				console.log("An error for user "+username);
-				badUser(socket);
-			}
-		});
-	} else {
-		badUser(socket);
-		console.log("User: "+username+" already exists");
-	}
-}
-
-function badUser(socket){
-	socket.emit("senduser", {name:'null',rooms:{}});
-}
-
-function userExists(username){
-	var ret = false;
-	User.findOne({name:username}, 'name', function(err, user){
-		if (err || !user){
-			ret = false;
-			console.log("User: "+username+" doesn't exist");
+	User.findOne({name:username}, 'name rooms', function(err, user){
+		if (!err && user.pass == passwd){
+			socket.emit("sendUser", {name:user.name, rooms:user.rooms});
+			io.emit("userLoggedIn", {name:user.name});
 		} else {
-			ret = true;
-			console.log("User: "+username+" exists");
+			socket.emit("sendUser", {name:"null", msg:err});
 		}
 	});
-	return ret;
+}
+
+function badUser(socket, msg){
+	socket.emit("senduser", {name:'null',rooms:{}, msg:msg});
 }
 
 function newRoom(roomname, users){
-	Room.create({name:roomname, log:[],users:[users]}, function(err, room){
+	Room.create({name:roomname, log:[],users:users}, function(err, room){
 		if (!err){
 			console.log("created room "+room.name);
 		} else {
 			console.log("couldn't create room "+room.name);
+		}
+	});
+}
+
+function addUserToRoom(username, roomname){
+	User.find({name: username}, 'name', function(err, user){
+		Room.find({name: roomname}, 'name', function(err, room){
+			user.rooms.push(room.name);
+			room.users.push(user.name);
+			user.save();
+			room.save();
+
+		});
+	});
+}
+
+function removeUserfromRoom(username, roomname){
+	User.find({name: username}, 'name rooms', function(err, user){
+		Room.find({name: roomname}, 'name users', function(err, room){
+			if (room.users.indexOf(user.name) >= 0){
+				delete room.users.indexOf(user.name);
+			}
+			if (user.rooms.indexOf(room.name) >= 0){
+				delete user.rooms.indexOf(room.name);
+			}
+			user.save();
+			room.save();
+			io.emit('userLeftRoom', user.name, room._id);
+		});
+	});
+}
+
+function createPublic(){
+	Room.create({name:"Public", users:{}}, function(err, room){
+		if (err){
+			console.log('create public failed: '+err);
+		} else {
+			console.log('created public');
 		}
 	});
 }
@@ -80,12 +110,17 @@ io.on("connection", function(socket){
 	socket.on('login', function(data){
 		getUser(data['name'], data['pass'], socket);
 	});
-	socket.on('dumblogin', function(data){
-		users.push(data['user']);
-		socket.emit('senduser', {'user':data['user'], 'users':users});
-	});
 	socket.on('messageToServer', function(data, room){
 		io.emit('messageFromServer', data, room);
+	});
+	socket.on('newRoom', function(room){
+		newRoom(room.name, room.users);
+	});
+	socket.on('leaveRoom', function(user, room){
+		removeUserfromRoom(user.name, room.name);
+	});
+	socket.on('addToRoom', function(user, room){
+		addUserToRoom(user.name, room.name);
 	});
 });
 
@@ -96,5 +131,12 @@ server.listen(1234, function() {
 			current.remove();
 		});
 	});
+	Room.find({}), function(err, rooms){
+		room.forEach(function(current){
+			console.log("bye "+current.name);
+			current.remove();
+		});
+	}
+	createPublic();
     console.log("Server is listening on port 1234");
 });
